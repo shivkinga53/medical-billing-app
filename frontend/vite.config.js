74274a92db01c137dd716cd3bb01f2a9110b62b1
Shivkumar Sharma <shivkinga53@gmail.com>
2025-08-26 21:06:59 +0530

    chores(cleanup): removed extra code

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,15 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	server: {
-		proxy: {
-			// String shorthand for simple cases
-			// Forward any request starting with /api to the backend server
-			"/api": {
-				target: "http://localhost:3001", // Your Express server's address
-				changeOrigin: true, // Recommended for virtual hosts
-				secure: false, // Optional: if your backend is not https
-			},
-		},
-	},
 });
